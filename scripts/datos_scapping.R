f9f94c0e1ec0ddf0de8dcb11851d3be73bd0ed7d
Gomezj1907 <102976529+Gomezj1907@users.noreply.github.com>
2023-09-16 18:41:37 -0500

    Approaches 2 y 3 de missings

--- scripts/datos_scapping.R
+++ scripts/datos_scapping.R
@@ -91,3 +91,16 @@ geih_clean <- geih %>%
   # Step 4: imputo los predichos
   geih1[is.na(geih1$wage), "wage"] <- predicted_values
   
+  # Approach 2: imputar promedio
+  
+  geih2$wage[is.na(geih2$wage)] <- mean(geih2$wage, na.rm = TRUE)
+  
+  # Approach 3: imputar mediana por cola a la derecha
+  
+  geih3$wage[is.na(geih3$wage)] <- median(geih3$wage, na.rm = TRUE)
+  
+  # Revisamos el proceso 
+  colSums(is.na(geih1)) # de 6650 a 837
+  colSums(is.na(geih2)) # de 6650 a 0
+  colSums(is.na(geih3)) # de 6650 a 0
+  
