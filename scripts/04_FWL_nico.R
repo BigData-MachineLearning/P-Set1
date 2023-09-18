20706ba5d60466142d168f4f5e64e8232e5531bf
nicolasgort <141876395+nicolasgort@users.noreply.github.com>
2023-09-17 19:55:09 -0500

    Update 04_FWL_nico.R

--- scripts/04_FWL_nico.R
+++ scripts/04_FWL_nico.R
@@ -9,21 +9,6 @@ p_load(rvest, tidyverse, knitr, kableExtra, readxl,
 geih_2018 <- read_csv("stores/geih_clean.csv")
 geih_clean <- read_csv("stores/geih_clean.csv")
 
-
-#Let's do bootstrap:
-
-geih_clean <- geih_clean%>% mutate(ln_wage=log(wage),
-                                 female=case_when(sex==1~0,
-                                                  sex==0~1))
-
-#We now run the model as proposed:
-
-model <- lm(ln_wage~sex, data=geih_2018) #We run the model using only wage and sex. Sex is a 
-# dummy variable which is 0 if female, 1 if male. 
-
-stargazer(model, type = 'text') # Without further analysis, we can see that preliminarily, being 
-#male tends to increase you wage in about 4.5% 
-
 # At this point, we need to decide which method we're going to use to deal with the missing values.
 
 colSums(is.na(geih_clean))
@@ -40,11 +25,11 @@ geih3 <- geih_clean #this data will be used to develope the third approach.
 
 # Step 1: divide data with missing and without missing values.
 
-data_with_missing <- geih1[is.na(geih1$wage), ]  # Rows with missing values
-data_without_missing <- geih1[!is.na(geih1$wage), ]  # Rows without missing values
+data_with_missing <- geih_clean[is.na(geih_clean$wage), ]  # Rows with missing values
+data_without_missing <- geih_clean[!is.na(geih_clean$wage), ]  # Rows without missing values
 
 # Step 2: regression in without missing values. 
-reg_1 <-  lm(wage ~ ., data = geih1 )
+reg_1 <-  lm(wage ~ ., data = geih_clean )
 
 
 # Step 3: Usage of the predict command to predict the missing values.
@@ -65,7 +50,22 @@ geih3$wage[is.na(geih3$wage)] <- median(geih3$wage, na.rm = TRUE)
 
 #After some discussion, we decided to use the third approach for this regard.
 
-geih_fwl <- geih2
+geih_final <- geih1
+
+
+#Let's do bootstrap:
+
+geih_final <- geih_final%>% mutate(ln_wage=log(wage),
+                                 female=case_when(sex==1~0,
+                                                  sex==0~1))
+
+#We now run the model as proposed:
+
+model <- lm(ln_wage~female, data=geih_final) #We run the model using only wage and female. Female is a 
+# dummy variable which is 1 if female, 0 if male. 
+
+stargazer(model, type = 'text')
+
 
 
 # Now, let's apply the FWL theorem.
@@ -74,7 +74,9 @@ geih_fwl <- geih2
 
 reg1 <- lm(female ~ poly(age,2,raw=TRUE)+ parentesco + p6210 + other_job + estrato1 +
              cotPension + regSalud + cuentaPropia + formal + poly(hoursWorkUsual,2,raw=TRUE) + 
-             oficio + firm_time + sizeFirm + iof1 + iof2 + iof6, data=geih_fwl)
+             oficio + firm_time + sizeFirm + iof1 + iof2 + iof6, data=geih_final)
+
+stargazer(reg1,type="text")
 
 #2. We save the residuals of the model above.
 residualsreg1 <- resid(reg1)
@@ -83,7 +85,9 @@ residualsreg1 <- resid(reg1)
 
 reg2 <- lm(ln_wage ~ poly(age,2,raw=TRUE)+ parentesco + p6210 + other_job + estrato1 +
              cotPension + regSalud + cuentaPropia + formal + poly(hoursWorkUsual,2,raw=TRUE) + 
-             oficio + firm_time + sizeFirm + iof1 + iof2 + iof6, data=geih_fwl)
+             oficio + firm_time + sizeFirm + iof1 + iof2 + iof6, data=geih_final)
+
+stargazer(reg2,type="text")
 
 #4. We save the residuals aswell.
 
@@ -91,11 +95,11 @@ residualsreg2 <- resid(reg2)
 
 #5. We regress both residuals:
 
-reg3 <- lm(residualsreg2 ~ residualsreg1, data=geih_fwl)
+reg3 <- lm(residualsreg2 ~ residualsreg1, data=geih_final)
 
+stargazer(model,reg3,type="text")
 
-
-
+#Let's to de FWL again, but using bootstrap.
 
 
 
